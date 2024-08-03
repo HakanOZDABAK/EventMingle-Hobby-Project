74a488b4e577053b53b10df8a58ae950ed006b34
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-08-04 01:40:19 +0300

    Event maker added

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,26 +1,52 @@
 "use client";
-import React, { useState } from "react";
-import { Dropdown } from 'primereact/dropdown';
-import { Button } from 'primereact/button';
+import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+import { Dropdown } from "primereact/dropdown";
+import { useState } from "react";
+import { useForm } from 'react-hook-form';
+import { InputText } from "primereact/inputtext";
 
 export default function Footer() {
   const [selectedCity, setSelectedCity] = useState<any>(null);
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = (data:any) => console.log(data);
   const cities = [
-      { name: 'New York', code: 'NY' },
-      { name: 'Rome', code: 'RM' },
-      { name: 'London', code: 'LDN' },
-      { name: 'Istanbul', code: 'IST' },
-      { name: 'Paris', code: 'PRS' }
+    { name: "New York", code: "NY" },
+    { name: "Rome", code: "RM" },
+    { name: "London", code: "LDN" },
+    { name: "Istanbul", code: "IST" },
+    { name: "Paris", code: "PRS" },
   ];
-
+  const [visible, setVisible] = useState<boolean>(false);
+  const footerContent = (
+    <div>
+      <Button
+        label="İptal Et"
+        icon="pi pi-times"
+        onClick={() => setVisible(false)}
+        className="p-button-text"
+      />
+      <Button
+        label="Oluştur"
+        icon="pi pi-check"
+        type="submit"
+        onClick={handleSubmit(onSubmit)}
+        autoFocus
+      />
+    </div>
+  );
   return (
-    <footer >
+    <footer>
       <div className="grid align-items-center justify-content-between">
         <div className="col-fixed ml-3">
           <div>HELLO</div>
         </div>
         <div className="pl-8 flex justify-content-center">
-          <Button label="Etkinlik Ekle" severity="success" />
+          <Button
+            label="Etkinlik Ekle"
+            severity="success"
+            onClick={() => setVisible(true)}
+          />
         </div>
         <div className="col-fixed mr-3">
           <Dropdown
@@ -33,6 +59,23 @@ export default function Footer() {
           />
         </div>
       </div>
+      <Dialog
+        header="Etkinlik Bilgileri"
+        visible={visible}
+        style={{ width: "25vw" }}
+        onHide={() => {
+          if (!visible) return;
+          setVisible(false);
+        }}
+        footer={footerContent}
+      >
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <InputText  type="text" placeholder="Etkinlik Adı" {...register("Etkinlik Adı", {})} />
+      <InputText type="datetime-local" placeholder="Etkinlik Zamanı" {...register("Etkinlik Zamanı", {})} />
+      <InputText {...register("Etkinlik Açıklaması", {})} />
+     
+    </form>
+      </Dialog>
     </footer>
   );
 }
