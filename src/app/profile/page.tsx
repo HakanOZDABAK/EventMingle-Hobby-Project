028a6d9a4ddf0cfb1550b84babb2f2549ab5fa26
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-08-04 13:01:14 +0300

    profile page created

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { Menu } from "primereact/menu";
+import { MenuItem } from "primereact/menuitem";
+import { TabMenu } from "primereact/tabmenu";
+
+import React from "react";
 
 export default function Profile() {
+  const profileMenu: MenuItem[] = [
+    { label: "Genel", icon: "pi pi-home" },
+    { label: "Etkinlikler", icon: "pi pi-chart-line" },
+    { label: "Ayarlar", icon: "pi pi-list" },
+  ];
+
   return (
-    <div>page</div>
-  )
+    <div>
+      <div className="grid">
+        <div className="col">
+          <TabMenu
+            className="flex justify-content-center"
+            model={profileMenu}
+          />
+        </div>
+      </div>
+      <div className="grid">
+        <div className="col">
+
+        </div>
+      </div>
+    </div>
+  );
 }
